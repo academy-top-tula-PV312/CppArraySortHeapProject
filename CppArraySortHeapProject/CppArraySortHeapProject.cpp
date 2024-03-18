abf13165ddf3624d0e866e4aed0e526ee6e69876
Максим Ефимов <efimov_m@t.top-academy.ru>
2024-03-18 21:16:32 +0300

    Добавьте файлы проекта.

---
+++ CppArraySortHeapProject/CppArraySortHeapProject.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+
+void ArrayPrint(int* array, int size)
+{
+    for (int i{}; i < size; i++)
+        std::cout << array[i] << " ";
+    std::cout << "\n";
+}
+
+void Swap(int& a, int& b)
+{
+    int temp{ a };
+    a = b;
+    b = temp;
+}
+
+void Heapify(int* array, int size, int index)
+{
+    
+    int left = index * 2 + 1;
+    int right = index * 2 + 2;
+
+    int largest = (array[left] < array[right]) ? right : left;
+    if (array[index] < array[largest])
+    {
+        Swap(array[index], array[largest]);
+        if (largest < size / 2 - 1)
+            Heapify(array, size, largest);
+    }
+}
+
+void ArraySortHeap(int* array, int size)
+{
+    for (int i{ size / 2 - 1 }; i >= 0; i--)
+        Heapify(array, size, i);
+    ArrayPrint(array, size);
+
+    for (int i{ size - 1 }; i >= 0; i--)
+    {
+        Swap(array[0], array[i]);
+        ArrayPrint(array, size);
+        Heapify(array, i, 0);
+        ArrayPrint(array, size);
+    }
+    
+}
+
+int main()
+{
+    srand(time(nullptr));
+
+    int size{ 7 };
+    int* array = new int[size];
+
+    for (int i{}; i < size; i++)
+        array[i] = rand() % 100;
+    ArrayPrint(array, size);
+
+    ArraySortHeap(array, size);
+    /*for (int i{ size / 2 - 1 }; i >= 0; i--)
+        Heapify(array, size, i);*/
+
+    ArrayPrint(array, size);
+}
